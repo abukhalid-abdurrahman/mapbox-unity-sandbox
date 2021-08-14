d4f0a8b90a66460a725b6da23aa76d14345189e5
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-14 06:32:26 +0500

    added response model

---
+++ New Unity Project/Assets/Scripts/Models/Response.cs
@@ -0,0 +1,10 @@
+﻿using Enums;
+
+namespace Models
+{
+    public class Response<T>
+    {
+        public ResponseStatusCode StatusCode { get; set; }
+        public T Payload { get; set; }
+    }
+}
