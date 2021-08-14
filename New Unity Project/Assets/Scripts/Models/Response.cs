b8a2ddc3e32aecc472e5209e3b4ec03c07095f46
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-14 06:59:48 +0500

    get vectore map implemented

--- New Unity Project/Assets/Scripts/Models/Response.cs
+++ New Unity Project/Assets/Scripts/Models/Response.cs
@@ -5,6 +5,14 @@ namespace Models
     public class Response<T>
     {
         public ResponseStatusCode StatusCode { get; set; }
+        public string Message { get; set; }
         public T Payload { get; set; }
+
+        public Response()
+        {
+            StatusCode = ResponseStatusCode.Success;
+            Message = "OK";
+            Payload = default;
+        }
     }
 }
--- New Unity Project/Assets/Scripts/Services/MapboxMaps/MapboxMapService.cs
+++ New Unity Project/Assets/Scripts/Services/MapboxMaps/MapboxMapService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Enums;
 using Models;
 using Models.Map;
 
@@ -9,15 +11,47 @@ namespace Services.MapboxMaps
     {
         private readonly string _mapBoxToken;
         private readonly string _baseUrl;
+
+        private readonly HttpClient _httpClient;
+        
         public MapboxMapService()
         {
             _baseUrl = "https://api.mapbox.com";
             _mapBoxToken = "pk.eyJ1IjoiZmFoYS1iZXJkaWV2IiwiYSI6ImNrczl3MTdvMzFhMDkyb3MwNzFlNTZpcmwifQ.aeRE3fuGsx2eyvArIoXPUg";
+            _httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(_baseUrl),
+                Timeout = TimeSpan.FromMinutes(1)
+            };
         }
 
         public async Task<Response<VectorTile>> GetVectorTileMap()
         {
-            throw new NotImplementedException();
+            var response = new Response<VectorTile>();
+            try
+            {
+                var tilesetId = "mapbox.mapbox-streets-v8";
+                var zoom = "1";
+                var x = "0";
+                var y = "0";
+                var format = "mvt";
+                var httpRequest = new HttpRequestMessage()
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"/v4/{tilesetId}/{zoom}/{x}/{y}.{format}?access_token={_mapBoxToken}")
+                };
+
+                var responseMessage = await _httpClient.SendAsync(httpRequest);
+                var responseBytes = await responseMessage.Content.ReadAsByteArrayAsync();
+                response.Payload.Bytes = responseBytes;
+                return response;
+            }
+            catch (Exception e)
+            {
+                response.StatusCode = ResponseStatusCode.Fail;
+                response.Message = e.Message;
+                return response;
+            }
         }
 
         public async Task<Response<RasterTile>> GetRasterTileMap()
