0b797c4ad1dcc99cddbd07192862c8cb4a8da61a
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-14 06:37:49 +0500

    added raster tile map

--- New Unity Project/Assets/Scripts/Services/MapboxMaps/IMapboxMapService.cs
+++ New Unity Project/Assets/Scripts/Services/MapboxMaps/IMapboxMapService.cs
@@ -1,7 +1,11 @@
+using System.Threading.Tasks;
+using Models.Map;
+
 namespace Services.MapboxMaps
 {
     public interface IMapboxMapService
     {
-    
+        Task<VectorTile> GetVectorTileMap();
+        Task<RasterTile> GetRasterTileMap();
     }
 }
