3bf0a67e868b65326d3ee74eeca4907e072b0da7
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-14 06:41:50 +0500

    added mapbox map implementation class

---
+++ New Unity Project/Assets/Scripts/Services/MapboxMaps/MapboxMapService.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Models.Map;
+
+namespace Services.MapboxMaps
+{
+    public class MapboxMapService : IMapboxMapService
+    {
+        public async Task<VectorTile> GetVectorTileMap()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<RasterTile> GetRasterTileMap()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<StaticImage> GetStaticImageMap()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<StaticTile> GetStaticTileMap()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
