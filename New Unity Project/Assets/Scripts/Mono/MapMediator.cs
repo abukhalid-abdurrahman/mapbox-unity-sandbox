0f473f7e9c5e905b9ac51dd9f349b26a2e0f22cb
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-14 07:49:44 +0500

    added map mono script

---
+++ New Unity Project/Assets/Scripts/Mono/MapMediator.cs
@@ -0,0 +1,32 @@
+﻿using Enums;
+using Services.MapboxMaps;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Mono
+{
+    public class MapMediator : MonoBehaviour
+    {
+        [FormerlySerializedAs("OutputTransform")] 
+        public Transform outputTransform;
+
+        private IMapboxMapService _mapboxMapService;
+
+        private void Start()
+        {
+            _mapboxMapService = new MapboxMapService();
+            var rasterImage = _mapboxMapService
+                .GetRasterTileMap().GetAwaiter().GetResult();
+            if (rasterImage.StatusCode == ResponseStatusCode.Success)
+            {
+                var tex = new Texture2D(2, 2);
+                tex.LoadImage(rasterImage.Payload.Bytes);
+                outputTransform.GetComponent<Renderer>().material.mainTexture = tex;
+            }
+            else
+            {
+                Debug.LogError(rasterImage.Message);
+            }
+        }
+    }
+}
