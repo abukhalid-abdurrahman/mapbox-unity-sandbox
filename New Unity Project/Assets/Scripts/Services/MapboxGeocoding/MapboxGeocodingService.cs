7591368b7fd6b2a1c431f294103fac3103e9fa94
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-14 11:08:43 +0500

    added geocoding class service

---
+++ New Unity Project/Assets/Scripts/Services/MapboxGeocoding/MapboxGeocodingService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Models;
+using Models.Geocoding;
+using Models.Requests;
+
+namespace Services.MapboxGeocoding
+{
+    public class MapboxGeocodingService : IMapboxGeocodingService
+    {
+        public async Task<Response<Geocoding>> GetForwardGeocoding(GetForwardGeocodingRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Response<Geocoding>> GetReverseGeocoding(GetReverseGeocodingRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Response<List<Geocoding>>> GetReverseBatchGeocoding(GetReverseBatchGeocodingRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Response<List<Geocoding>>> GetForwardBatchGeocoding(GetForwardBatchGeocodingRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
