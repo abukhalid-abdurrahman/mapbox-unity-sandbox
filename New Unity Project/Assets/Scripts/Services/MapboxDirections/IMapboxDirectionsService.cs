4b8c87b53cee0bfab2de80899b11abcbb82f8422
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-15 08:16:24 +0500

    MapboxDirectionsService added

--- New Unity Project/Assets/Scripts/Services/MapboxDirections/IMapboxDirectionsService.cs
+++ New Unity Project/Assets/Scripts/Services/MapboxDirections/IMapboxDirectionsService.cs
@@ -1,7 +1,12 @@
-﻿namespace Services.MapboxDirections
+﻿using System.Threading.Tasks;
+using Models;
+using Models.Direction;
+using Models.Requests;
+
+namespace Services.MapboxDirections
 {
     public interface IMapboxDirectionsService
     {
-        
+        Task<Response<Direction>> RetrieveDirections(GetDirectionRequest request);
     }
 }
---
+++ New Unity Project/Assets/Scripts/Services/MapboxDirections/MapboxDirectionsService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Enums;
+using Extensions;
+using Models;
+using Models.Direction;
+using Models.Requests;
+using Newtonsoft.Json;
+
+namespace Services.MapboxDirections
+{
+    public class MapboxDirectionsService : IMapboxDirectionsService
+    {
+        private readonly string _mapBoxToken;
+        private readonly string _baseUrl;
+
+        private readonly HttpClient _httpClient;
+        
+        public MapboxDirectionsService()
+        {
+            _baseUrl = "https://api.mapbox.com/";
+            _mapBoxToken = "pk.eyJ1IjoiZmFoYS1iZXJkaWV2IiwiYSI6ImNrczl3MTdvMzFhMDkyb3MwNzFlNTZpcmwifQ.aeRE3fuGsx2eyvArIoXPUg";
+            _httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(_baseUrl),
+                Timeout = TimeSpan.FromMinutes(1)
+            };
+        }
+        
+        public async Task<Response<Direction>> RetrieveDirections(GetDirectionRequest request)
+        {
+            if (request == null)
+                throw new ArgumentNullException();
+            
+            var response = new Response<Direction>();
+            
+            try
+            {
+                var url = $"directions/v5/{request.RoutingProfile.GetDescription()}/{string.Join(";",request.Coordinates)}";
+                var requestMessage = new HttpRequestMessage()
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri(_httpClient.BaseAddress + url)
+                };
+                var responseMessage = await _httpClient.SendAsync(requestMessage);
+                if (!responseMessage.IsSuccessStatusCode)
+                {
+                    response.StatusCode = ResponseStatusCode.Fail;
+                    response.Message = responseMessage.ReasonPhrase;
+                    return response;
+                }
+                var responseContent = await responseMessage.Content.ReadAsStringAsync();
+                var entity = JsonConvert.DeserializeObject<Direction>(responseContent);
+                response.Payload = entity;
+                return response;
+            }
+            catch (Exception e)
+            {
+                response.Message = e.Message;
+                response.StatusCode = ResponseStatusCode.Fail;
+                return response;
+            }
+        }
+    }
+}
